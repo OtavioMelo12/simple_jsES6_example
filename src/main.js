196bc1956f2df5d78195548c229eaea2b9c7f307
OtavioMelo12 <otavio.souza@estudante.ufla.br>
2019-09-25 14:55:55 -0300

    exibindo loading e mensagem de erro

--- src/main.js
+++ src/main.js
@@ -16,6 +16,18 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
+    setLoading (loading=true) {
+        if(loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('CARREGANDO...'))
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        }else {
+            document.getElementById('loading').remove();
+        }
+    }
+
     async addRepository(event) {
         event.preventDefault();
 
@@ -25,21 +37,28 @@ class App {
             return;
         }
 
-        const response = await api.get(`/repos/${repoInput}`);
+        this.setLoading();
 
-        const { name, description, html_url, owner:{ avatar_url } } = response.data;
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const { name, description, html_url, owner:{ avatar_url } } = response.data;
 
 
-        this.repositories.push({
-            name,
-            description,
-            avatar_url,
-            html_url,
-        });
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
 
+            this.render();
+        } catch(err){
+            alert("REPOSITÓRIO NÃO ENCONTRADO")
+        }
+        
         this.inputEl.value = '';
-
-        this.render();
+        this.setLoading(false);
     }
 
     render() {
